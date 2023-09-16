3a77e042c9092b826dd4f857f461641c0ecfefe6
livia <liviapadovine@hotmail.com>
2023-09-16 12:54:42 -0300

    Primeira versão

---
+++ src/test/java/br/bosch/CadastroUsuario/CadastroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.bosch.CadastroUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
